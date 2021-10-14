6f17db7a3784921e57a18385cec6553a2fc01b22
Rickard Åberg <raberg@Rickards-MacBook-Pro.local>
2021-10-14 17:32:37 +0200

    rest api get /1-4 impl

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # rest-api-node
 A Node implementation of rest-api for storytel interview
+
+# url : http://localhost:3000/api/messages
+# get - ger en json
+
+
+# post - ger en json ifall idet skickas in
+# delete - tar bort
+# put ändrar?
+
+# testa med curl och sen plugin i visual code
+# gör enhets-tester 
+
+# skickar jag in en body till 
+
+# servern svarar utifrån vad som är i body, url och headern
+# vi kan t.ex. göra en http get genom att
+# skicka in url: .../?id='1'
+
+# eller om vi skickar en post med en klient, så fattar rest-apiet det
+# var sätts get post osv? är det i headern?
+# minns inte
+
--- server.js
+++ server.js
@@ -1,5 +1,60 @@
 const express = require('express');
 //import "express.js"
+const app = express();
+const debug = require('debug')('http');
 
+if (debug.enabled) {
+  console.log("debug enabled")
+}
 
+app.set('view engine', 'pug');
 console.log("Hello server")
+
+
+app.get("/messages", (req, res) => {
+    res.send(data);
+})
+
+
+app.get("/messages/1", (req, res) => {
+  res.send(data["1"]);
+})
+
+app.get("/messages/2", (req, res) => {
+  res.send(data["2"]);
+})
+
+app.get("/messages/3", (req, res) => {
+  res.send(data["3"]);
+})
+
+app.get("/messages/4", (req, res) => {
+  res.send(data["4"]);
+})
+var data = {
+  "1" : "Message #1",
+  "2" : "Message #2",
+  "3" : "Message #3",
+  "4" : "Message #4"
+};
+
+// skilj på map key:value pair
+// och arrayer ---- index => data
+// vad vill jag ha här?
+//
+  // debug(req.method + ' ' + req.url);
+  //   res.send("Hello world");
+  //   res.render('index', {
+  //       title: "Titel",
+  //       message : "hej baberibba"
+  //   } )});
+
+app.listen(3000);
+
+
+// för att göra en post så måste man skicka in något
+// hur ser det ut?
+
+//app.post
+//app.delete
+//app.put
